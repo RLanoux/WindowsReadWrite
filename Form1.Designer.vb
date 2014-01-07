bb463f3fb591b934770f8a29ccea585e353c6930
Raymond Lanoux <raymondlanoux@gmail.com>
2014-01-06 18:44:32 -0600

    initial add

---
+++ Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtWord = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.cboWords = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'txtWord
+        '
+        Me.txtWord.Location = New System.Drawing.Point(12, 12)
+        Me.txtWord.Name = "txtWord"
+        Me.txtWord.Size = New System.Drawing.Size(105, 20)
+        Me.txtWord.TabIndex = 0
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(123, 12)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(108, 20)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "Add Word"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(123, 38)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(108, 20)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'cboWords
+        '
+        Me.cboWords.FormattingEnabled = True
+        Me.cboWords.Location = New System.Drawing.Point(12, 38)
+        Me.cboWords.Name = "cboWords"
+        Me.cboWords.Size = New System.Drawing.Size(105, 21)
+        Me.cboWords.Sorted = True
+        Me.cboWords.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(470, 427)
+        Me.Controls.Add(Me.cboWords)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtWord)
+        Me.Name = "Form1"
+        Me.Text = "Read and Write Text Files"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtWord As System.Windows.Forms.TextBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents cboWords As System.Windows.Forms.ComboBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,56 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private msFilePath As String = "C:/ReadWrite/WordsList.txt"
+    Private moSR As StreamReader = Nothing
+    Private moSW As StreamWriter = Nothing
+
+    Private Sub LoadWords()
+
+        cboWords.Items.Clear()
+
+        'Open the StremReader
+        moSR = New StreamReader(msFilePath)
+
+        While Not moSR.EndOfStream
+
+            cboWords.Items.Add(moSR.ReadLine())
+
+
+        End While
+
+        moSR.Close()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        If File.Exists(msFilePath) Then
+            LoadWords()
+        End If
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As System.Object, e As System.EventArgs) Handles btnExit.Click
+        End
+    End Sub
+
+    Private Sub btnAdd_Click(sender As System.Object, e As System.EventArgs) Handles btnAdd.Click
+        'Open the StreamWriter
+        moSW = New StreamWriter(msFilePath, True)
+
+        'Read the word out of the textbox and add the word to the text file
+        moSW.WriteLine(txtWord.Text)
+        moSW.Close()
+
+        'Load the combobox with the words from the text file
+        LoadWords()
+
+        'Clear the textbox
+        txtWord.Clear()
+
+        'Focus on the textbos
+        txtWord.Focus()
+
+    End Sub
+End Class
